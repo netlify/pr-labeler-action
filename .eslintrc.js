1775270c8f9ca1739b68834d7a65b9fbb3773517
erezrokah <erezrokah@users.noreply.github.com>
2021-06-07 13:10:54 +0200

    chore: fix lint errors, switch to npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
